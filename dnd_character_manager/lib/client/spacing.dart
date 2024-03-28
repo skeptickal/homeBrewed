25adf5f8e0d26bffbb276c94d5ea5339ec1dbad1
Jackson Geer <jacksongeer@gmail.com>
2024-03-28 15:25:40 -0400

    added character view design

---
+++ dnd_character_manager/lib/client/spacing.dart
@@ -0,0 +1,10 @@
+import 'package:dnd_character_manager/constants/theme_data.dart';
+import 'package:flutter/material.dart';
+
+SizedBox seperation = const SizedBox(height: 10);
+Container horizontalLine = Container(
+  padding: const EdgeInsets.only(top: 10),
+  decoration: BoxDecoration(
+    border: Border(bottom: BorderSide(color: blueGrey)),
+  ),
+);
