da987f314d11c60e92a772735fb8fe0e7b90b73b
Jackson Geer <jacksongeer@gmail.com>
2024-04-09 12:32:26 -0400

    ReactiveFormBuilder has saved my life

--- dnd_character_manager/lib/pages/edit_popups/edit_item_screen.dart
+++ dnd_character_manager/lib/pages/edit_popups/edit_item_screen.dart
@@ -1,115 +1,69 @@
-import 'package:dnd_character_manager/constants/text_fields.dart';
 import 'package:dnd_character_manager/constants/theme_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 
 import '../../cubits/item_cubit/item_cubit.dart';
 import '../../models/item.dart';
 
 class EditItemScreen extends StatelessWidget {
   final Item item;
-  const EditItemScreen({super.key, required this.item});
+
+  const EditItemScreen({Key? key, required this.item}) : super(key: key);
+  FormGroup buildForm() => fb.group(<String, Object>{
+        'name': FormControl<String>(value: item.name),
+        'amount': FormControl<String>(value: item.amount),
+        'description': FormControl<String>(value: item.description),
+      });
 
   @override
   Widget build(BuildContext context) {
-    print('Jeff ${item.itemID}');
-    TextEditingController name = TextEditingController(text: item.name);
-    TextEditingController amount = TextEditingController(text: item.amount);
-    TextEditingController description = TextEditingController(text: item.description);
-
     return BlocBuilder<ItemCubit, ItemState>(
       builder: (context, state) {
-        return Form(
-          child: SingleChildScrollView(
-            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-            padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-            child: Column(
-              children: [
-                BigTextBox(
-                  onTapOutside: (clickOut) {
-                    context.read<ItemCubit>().setItemsData(item.copyWith(
-                          name: name.text,
-                          description: description.text,
-                          amount: amount.text,
-                        ));
-                    context.read<ItemCubit>().readItemsByCharID(item.charID);
-                  },
-                  onEditingComplete: () {
-                    context.read<ItemCubit>().setItemsData(item.copyWith(
-                          name: name.text,
-                          description: description.text,
-                          amount: amount.text,
-                        ));
-                    context.read<ItemCubit>().readItemsByCharID(item.charID);
-                  },
-                  enabled: true,
-                  padding: const EdgeInsets.all(6),
-                  controller: name,
-                  hintText: 'Item name',
-                  subtitle: 'Item name',
-                ),
-                StatTextBox(
-                  onTapOutside: (clickOut) {
-                    context.read<ItemCubit>().setItemsData(item.copyWith(
-                          name: name.text,
-                          description: description.text,
-                          amount: amount.text,
-                        ));
-                    context.read<ItemCubit>().readItemsByCharID(item.charID);
-                  },
-                  onEditingComplete: () {
-                    context.read<ItemCubit>().setItemsData(item.copyWith(
-                          name: name.text,
-                          description: description.text,
-                          amount: amount.text,
-                        ));
-                    context.read<ItemCubit>().readItemsByCharID(item.charID);
-                  },
-                  enabled: true,
-                  padding: const EdgeInsets.symmetric(horizontal: 80),
-                  controller: amount,
-                  hintText: '2',
-                  subtitle: 'Amount',
-                  maxLength: 3,
-                ),
-                BigTextBox(
-                  onTapOutside: (clickOut) {
-                    context.read<ItemCubit>().setItemsData(item.copyWith(
-                          name: name.text,
-                          description: description.text,
-                          amount: amount.text,
-                        ));
-                    context.read<ItemCubit>().readItemsByCharID(item.charID);
-                  },
-                  onEditingComplete: () {
-                    context.read<ItemCubit>().setItemsData(item.copyWith(
-                          name: name.text,
-                          description: description.text,
-                          amount: amount.text,
-                        ));
-                    context.read<ItemCubit>().readItemsByCharID(item.charID);
-                  },
-                  enabled: true,
-                  padding: const EdgeInsets.all(6),
-                  controller: description,
-                  hintText: 'Has a chance to burn target',
-                  subtitle: 'Description (Optional)',
-                  minLines: 5,
-                ),
-                TextButton(
-                  onPressed: () {
-                    context.read<ItemCubit>().readItemsByCharID(item.charID);
-                    context.pop();
-                  },
-                  child: Text(
-                    'Done Editing',
-                    style: dndFont.copyWith(color: black),
+        return ReactiveFormBuilder(
+          form: buildForm,
+          builder: (context, formGroup, child) {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  ReactiveTextField<String>(
+                    formControlName: 'name',
+                    decoration: InputDecoration(labelText: 'Item Name', labelStyle: dndFont),
                   ),
-                ),
-              ],
-            ),
-          ),
+                  ReactiveTextField<String>(
+                    formControlName: 'amount',
+                    decoration: InputDecoration(labelText: 'Amount', labelStyle: dndFont),
+                  ),
+                  ReactiveTextField<String>(
+                    minLines: 1,
+                    maxLines: 10,
+                    formControlName: 'description',
+                    decoration: InputDecoration(labelText: 'Description', labelStyle: dndFont),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Item updatedItem = Item(
+                        itemID: item.itemID,
+                        charID: item.charID,
+                        name: formGroup.control('name').value,
+                        amount: formGroup.control('amount').value,
+                        description: formGroup.control('description').value,
+                      );
+
+                      context.read<ItemCubit>().setItemsData(updatedItem);
+                      context.read<ItemCubit>().readItemsByCharID(item.charID);
+                      context.pop();
+                    },
+                    child: Text(
+                      'Save Item',
+                      style: dndFont.copyWith(color: themeColor),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
         );
       },
     );
